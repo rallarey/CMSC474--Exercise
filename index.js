329e21e828dc30275feb1ea508872071379b2a15
Ryan Allarey <60989959+rallarey@users.noreply.github.com>
2021-10-19 11:05:42 -0400

    finished exercise 3

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 $( document ).ready(function() {
     console.log( "ready!" );
-    });
+    $('#go').on("click", function (e) {
+        e.preventDefault()
+        let first = $("#fname").val();
+        let last = $("#lname").val();
+        $("#jumbotron").append("Welcome " + first + " " + last + "! ")
+    })
+});
